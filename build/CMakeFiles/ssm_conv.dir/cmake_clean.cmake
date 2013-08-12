b8b364b74212b653953b891d6acbed690504e5c0
tema <tema@tema-System-Product-Name.(none)>
2013-08-12 19:44:50 +0400

    2

---
+++ build/CMakeFiles/ssm_conv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ssm_conv.dir/ssm_conv.c.o"
+  "libssm_conv.pdb"
+  "libssm_conv.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang C)
+  INCLUDE(CMakeFiles/ssm_conv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
